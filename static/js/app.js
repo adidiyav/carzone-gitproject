15dd2cbf603d790ae39a2a31e4c51c9f58b920dc
Diya V <diyav2015@gmail.com>
2021-04-03 13:48:48 +0530

    Added Cars app and fetched the data into homepage

--- static/js/app.js
+++ static/js/app.js
@@ -108,19 +108,19 @@ $(function () {
                     $('.sticky-header').addClass('header-shrink');
                 }
                 if($('.do-sticky').length < 1) {
-                    $('.company-logo img').attr('src', 'img/logos/black-logo.png');
+                    $('.company-logo img').attr('src', 'static/img/logos/black-logo.png');
                 }
             }
             else {
                 $('.sticky-header').removeClass('header-shrink');
                 if($('.do-sticky').length < 1 && $('.fixed-header').length == 0 && $('.fixed-header2').length == 0) {
-                    $('.company-logo img').attr('src', 'img/logos/logo.png');
+                    $('.company-logo img').attr('src', 'static/img/logos/logo.png');
                 } else {
-                    $('.company-logo img').attr('src', 'img/logos/black-logo.png');
+                    $('.company-logo img').attr('src', 'static/img/logos/black-logo.png');
                 }
             }
         } else {
-            $('.company-logo img').attr('src', 'img/logos/black-logo.png');
+            $('.company-logo img').attr('src', 'static/img/logos/black-logo.png');
         }
     }
 
@@ -227,7 +227,7 @@ $(function () {
     // Countdown activation
     $( function() {
         // Add background image
-        //$.backstretch('../img/nature.jpg');
+        //$.backstretch('../static/img/nature.jpg');
         var endDate = "December  27, 2019 15:03:25";
         $('.countdown.simple').countdown({ date: endDate });
         $('.countdown.styled').countdown({
@@ -320,7 +320,7 @@ $(function () {
     $(document).ready(function () {
         $(".player").mb_YTPlayer(
             {
-                mobileFallbackImage: 'img/banner/banner-1.jpg'
+                mobileFallbackImage: 'static/img/banner/banner-1.jpg'
             }
         );
     });
@@ -392,7 +392,7 @@ $(function () {
             centerMode: true,
             centerPadding: '0'
         });
-        
+
         $(this).closest('.slick-slider-area').find('.slick-prev').on("click", function () {
             slider.slick('slickPrev');
         });
