5dfccb4ebe427b6b42670f8fd5ad11f5fd255c81
Diya V <diyav2015@gmail.com>
2021-04-26 13:26:51 +0530

    changed runtime

--- carzone/settings.py
+++ carzone/settings.py
@@ -15,7 +15,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
-
 import dj_databse_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
